bcf20f94cf237ba0fac5969a319e0bdfd5060a8a
nazimfeni <mdnazimuddin77@gmail.com>
2024-02-03 15:13:31 +0600

    first commit

---
+++ todolist/src/App.jsx
@@ -0,0 +1,20 @@
+
+import { useState } from 'react'
+import './App.css'
+
+import Home from './Home'
+
+import './App.css'
+
+function App() {
+  
+  return (
+
+     <div className='home'>
+      <Home />
+     </div>
+   
+  )
+}
+
+export default App
---
+++ todolist/src/Create.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div >
+      <input type="text" name="" id=""  className='create_form_input'/>
+      <button type="button" className='create_form_button'>Add</button>
+
+    </div>
+  )
+}
+
+export default Create
