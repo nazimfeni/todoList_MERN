bc16787b27df8f960cfa758f8229e1ee409a286d
nazimfeni <mdnazimuddin77@gmail.com>
2024-02-05 16:27:58 +0600

    Add edit and delete option

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.get('/get', (req, res) => {
 
 
 
+
 app.post('/add', (req, res) => {
       const task = req.body.task;
       TodoModel.create({
@@ -29,6 +30,32 @@ app.post('/add', (req, res) => {
       .catch(err => res.json(err))
 
 })
+
+
+app.put('/update/:id', (req, res) => {
+      
+      const {id} = req.params;
+      // console.log(id);
+      TodoModel.findByIdAndUpdate({_id: id}, {done: true})
+      .then(result => res.json(result))
+      .catch(err => res.json(err))
+
+
+})
+
+
+app.delete('/delete/:id', (req, res) => {
+      
+      const {id} = req.params;
+      // console.log(id);
+      TodoModel.findByIdAndDelete({_id: id})
+      .then(result => res.json(result))
+      .catch(err => res.json(err))
+
+
+})
+
+
 
 
 app.listen(3001, () => {
--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -6,7 +6,8 @@ function Create() {
   const [task, setTask] = useState()
 const handleAdd = () => {
   axios.post('http://localhost:3001/add', {task:task})
-  .then(result => console.log(result))
+  .then(result => {location.reload()}
+  )
   .catch(err => console.log(err))
     
 }
